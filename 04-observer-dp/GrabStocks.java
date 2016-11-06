5f6a06ac1489dd494831c2d16e36ea6476a1d523
Yaroslav Prodanchuk <yprodanchuk@gmail.com>
2016-11-06 22:35:23 +0200

    Observer pattern using Threads

--- 04-observer-dp/GrabStocks.java
+++ 04-observer-dp/GrabStocks.java
@@ -20,5 +20,13 @@ public class GrabStocks {
         stockGrabber.setIBMPrice(197.00);
         stockGrabber.setAAPLPrice(677.70);
         stockGrabber.setGOOGPrice(676.40);
+
+        Runnable getIBM = new GetTheStock(stockGrabber, 2, "IBM", 197.00);
+        Runnable getAAPL = new GetTheStock(stockGrabber, 2, "AAPL", 677.70);
+        Runnable getGOOG = new GetTheStock(stockGrabber, 2, "GOOG", 676.40);
+
+        new Thread(getIBM).start();
+        new Thread(getAAPL).start();
+        new Thread(getGOOG).start();
     }
 }
