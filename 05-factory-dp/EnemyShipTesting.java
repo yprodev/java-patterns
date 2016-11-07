69a69b7d8c3bcfbe805c52f4161e80dcc0d05456
Yaroslav Prodanchuk <yprodanchuk@gmail.com>
2016-11-07 21:17:31 +0200

    Factory pattern in more dynamic way, but not secure enough

--- 05-factory-dp/EnemyShipTesting.java
+++ 05-factory-dp/EnemyShipTesting.java
@@ -5,7 +5,6 @@ public class EnemyShipTesting {
 	/*
 	 * Old style of implementing these things
 	 * This is not very good because it is NOT dynamic
-	 */
 	public static void main(String[] args) {
 		EnemyShip ufoShip = new UFOEnemyShip();
 
@@ -18,5 +17,33 @@ public class EnemyShipTesting {
 		anEnemyShip.followHeroShip();
 		anEnemyShip.enemyShipShoots();
 	}
+	*/
 
+	public static void main(String[] args) {
+
+		EnemyShip theEnemy = null;
+
+		Scanner userInput = new Scanner(System.in);
+		String enemyShipOption = "";
+		System.out.println("What type of ship (U / R): ");
+
+		if (userInput.hasNextLine()) {
+			enemyShipOption = userInput.nextLine();
+		}
+
+		if (enemyShipOption.equals("U")){
+			theEnemy = new UFOEnemyShip();
+		} else if (enemyShipOption.equals("R")) {
+			theEnemy = new RocketEnemyShip();
+		}
+
+		doStuffEnemy(theEnemy);
+	}
+
+	
+	public static void doStuffEnemy(EnemyShip anEnemyShip) {
+		anEnemyShip.displayEnemyShip();
+		anEnemyShip.followHeroShip();
+		anEnemyShip.enemyShipShoots();
+	}
 }
--- 05-factory-dp/RocketEnemyShip.java
+++ 05-factory-dp/RocketEnemyShip.java
@@ -2,6 +2,6 @@ public class RocketEnemyShip extends EnemyShip {
 
 	public RocketEnemyShip() {
 		setName("Rocket Enemy Ship");
-		setDamage(10.0);
+		setAmtDamage(10.0);
 	}
 }
