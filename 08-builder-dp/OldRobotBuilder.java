44c7d38d301fb4963b7b4cced007792145926a47
Yaroslav Prodanchuk <yprodanchuk@gmail.com>
2016-11-13 17:22:32 +0200

    Learned Builder DP

---
+++ 08-builder-dp/OldRobotBuilder.java
@@ -0,0 +1,17 @@
+public class OldRobotBuilder implements RobotBuilder {
+	private Robot robot;
+
+	public OldRobotBuilder() {
+		this.robot = new Robot();
+	}
+
+	public void buildRobotHead(){ robot.setRobotHead("Tin Head"); }
+	public void buildRobotTorso(){ robot.setRobotTorso("Tin Torso"); }
+	public void buildRobotArms(){ robot.setRobotArms("Blowtorch Arms"); }
+	public void buildRobotLegs(){ robot.setRobotLegs("Roller skates"); }
+
+	public Robot getRobot() {
+		return this.robot;
+	}
+}
+
