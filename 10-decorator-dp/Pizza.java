6da8598048d865628e5e7fce318dbdb26db3c0b8
Yaroslav Prodanchuk <yprodanchuk@gmail.com>
2016-11-19 20:33:01 +0200

    Wrong way of implementing Decorator

---
+++ 10-decorator-dp/Pizza.java
@@ -0,0 +1,6 @@
+public abstract class Pizza {
+	public abstract void setDescription(String newDescription);
+	public abstract String getDescription();
+	public abstract double getCost();
+	public abstract boolean hasFontina();
+}
---
+++ 10-decorator-dp/ThreeCheesePizza.java
@@ -0,0 +1,15 @@
+public class ThreeCheesePizza extends Pizza {
+
+	public void setDescription(String newDescription) {
+
+	};
+
+	public String getDescription() {
+		return "Mozzarella, Fontina, Parmesan, Cheese Pizza";
+	};
+
+	public double getCost() {
+		return 10.00;
+	};
+
+}
