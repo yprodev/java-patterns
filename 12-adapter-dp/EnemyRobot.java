2fb69eaea925b98b7310ffb4de82b98e7e26f850
Yaroslav Prodanchuk <yprodanchuk@gmail.com>
2016-11-20 18:18:30 +0200

    Learned Adapter DP

---
+++ 12-adapter-dp/EnemyRobot.java
@@ -0,0 +1,20 @@
+import java.util.Random;
+
+// Adaptee
+public class EnemyRobot {
+	Random generator = new Random();
+
+	public void smashWithHands() {
+		int attackDamage = generator.nextInt(10) + 1;
+		System.out.println("Enemy Robot Causes " + attackDamage + " Damage With Its' Hands");
+	}
+
+	public void walkForward() {
+		int movement = generator.nextInt(5) + 1;
+		System.out.println("Enemy Robot Walks Forward " + movement + " Spaces");
+	}
+
+	public void reactToHuman(String driverName) {
+		System.out.println("Enemy Robot Tramps On " + driverName);
+	}
+}
---
+++ 12-adapter-dp/EnemyRobotAdapter.java
@@ -0,0 +1,20 @@
+public class EnemyRobotAdapter implements EnemyAttacker {
+
+	EnemyRobot theRobot;
+
+	public EnemyRobotAdapter(EnemyRobot newRobot) {
+		theRobot = newRobot;
+	}
+
+	public void fireWeapon() {
+		theRobot.smashWithHands();
+	}
+
+	public void driveForward() {
+		theRobot.walkForward();
+	}
+
+	public void assignDriver(String driverName) {
+		theRobot.reactToHuman(driverName);
+	}
+}
