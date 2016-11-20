7ec18f425be6ea1b3ec5bc94067d8079ab2c42c4
Yaroslav Prodanchuk <yprodanchuk@gmail.com>
2016-11-20 20:52:49 +0200

    Learned Facade DP

---
+++ 13-facade-dp/WelcomeToBank.java
@@ -0,0 +1,6 @@
+public class WelcomeToBank {
+	public WelcomeToBank() {
+		System.out.println("Welcome to ABC bank!");
+		System.out.println("We are happy to give you your money if we can find it");
+	}
+}
